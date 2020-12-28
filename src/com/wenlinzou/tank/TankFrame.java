88b96b1ffc0a08d5fc07f59575f67adaa880a78f
wenlinzou <Daoxiji2012>
2020-12-28 13:28:49 +0800

    添加键盘监听事件

--- src/com/wenlinzou/tank/TankFrame.java
+++ src/com/wenlinzou/tank/TankFrame.java
@@ -1,6 +1,8 @@
 package com.wenlinzou.tank;
 
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -20,6 +22,8 @@ public class TankFrame extends Frame {
         setTitle("tank war");
         setVisible(true);
 
+        this.addKeyListener(new MyKeyListener());
+
         addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -34,5 +38,18 @@ public class TankFrame extends Frame {
         g.fillRect(x, y, 50, 50);
         x += 10;
         y += 10;
+    }
+
+    class MyKeyListener extends KeyAdapter {
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            System.out.println("key pressed");
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            System.out.println("key released");
+        }
     }
 }

962b40d15b9ee22cced1c05d7a64af0bc15b61c8
wenlinzou <Daoxiji2012>
2020-12-28 13:05:23 +0800

    Frame类继承，重写paint方法，画个方块

--- src/com/wenlinzou/tank/T.java
+++ src/com/wenlinzou/tank/T.java
@@ -1,9 +1,5 @@
 package com.wenlinzou.tank;
 
-import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
 /**
  * description：
  *
@@ -12,17 +8,6 @@ import java.awt.event.WindowEvent;
  */
 public class T {
     public static void main(String[] args) {
-        Frame f = new Frame();
-        f.setSize(800, 600);
-        f.setResizable(false);
-        f.setTitle("tank war");
-        f.setVisible(true);
-
-        f.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
+        TankFrame tankFrame = new TankFrame();
     }
 }

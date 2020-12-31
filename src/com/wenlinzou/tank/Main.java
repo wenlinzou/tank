42f3826239ab56a1b076ab6f34d36707fb18d677
wenlinzou <Daoxiji2012>
2020-12-31 12:31:54 +0800

    添加键盘处理，根据上下左右箭头的按键状态，判定坦克的移动方向

--- src/com/wenlinzou/tank/Main.java
+++ src/com/wenlinzou/tank/Main.java
@@ -7,7 +7,12 @@ package com.wenlinzou.tank;
  * @date 2020/12/28
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         TankFrame tankFrame = new TankFrame();
+
+        while (true) {
+            Thread.sleep(50);
+            tankFrame.repaint();
+        }
     }
 }

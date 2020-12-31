b3461f7983df2629e9d9b0beae8e4f696ee5aadd
wenlinzou <Daoxiji2012>
2020-12-31 13:38:03 +0800

    添加tank的静止的处理，只要4个键都没有按下去，tank就是静止的

--- src/com/wenlinzou/tank/Tank.java
+++ src/com/wenlinzou/tank/Tank.java
@@ -13,6 +13,8 @@ public class Tank {
     private Dir dir = Dir.DOWN;
     private static final int SPEED = 10;
 
+    private boolean moving = false;
+
     public Tank(int x, int y, Dir dir) {
         super();
         this.x = x;
@@ -28,9 +30,23 @@ public class Tank {
         this.dir = dir;
     }
 
+    public boolean isMoving() {
+        return moving;
+    }
+
+    public void setMoving(boolean moving) {
+        this.moving = moving;
+    }
+
     public void paint(Graphics g) {
         g.fillRect(x, y, 50, 50);
 
+        move();
+
+    }
+
+    private void move() {
+        if (!moving) {return;}
         switch (dir) {
             case LEFT:
                 x -= SPEED;
--- src/com/wenlinzou/tank/TankFrame.java
+++ src/com/wenlinzou/tank/TankFrame.java
@@ -91,18 +91,24 @@ public class TankFrame extends Frame {
         }
 
         private void setMainTankDir() {
-            if (bL) {
-                myTank.setDir(Dir.LEFT);
+            if (!bL && !bU && !bR && !bD) {
+                myTank.setMoving(false);
+            } else {
+                myTank.setMoving(true);
+                if (bL) {
+                    myTank.setDir(Dir.LEFT);
+                }
+                if (bU) {
+                    myTank.setDir(Dir.UP);
+                }
+                if (bR) {
+                    myTank.setDir(Dir.RIGHT);
+                }
+                if (bD) {
+                    myTank.setDir(Dir.DOWN);
+                }
             }
-            if (bU) {
-                myTank.setDir(Dir.UP);
-            }
-            if (bR) {
-                myTank.setDir(Dir.RIGHT);
-            }
-            if (bD) {
-                myTank.setDir(Dir.DOWN);
-            }
+
         }
 
     }

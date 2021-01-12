8388e9cc2335ecca1a702a1d453ed42d277b3e22
wenlinzou <Daoxiji2012>
2021-01-12 21:01:26 +0800

    区分敌我坦克的图片

--- src/com/wenlinzou/tank/Tank.java
+++ src/com/wenlinzou/tank/Tank.java
@@ -14,8 +14,9 @@ public class Tank {
     private Dir dir = Dir.DOWN;
     private static final int SPEED = 1;
 
-    public static final int WIDTH = ResourceMgr.tankD.getWidth();
-    public static final int HEIGHT = ResourceMgr.tankD.getHeight();
+    public static final int WIDTH = ResourceMgr.goodTankU.getWidth();
+
+    public static final int HEIGHT = ResourceMgr.goodTankU.getHeight();
 
     private Random random = new Random();
 
@@ -78,18 +79,19 @@ public class Tank {
             tankFrame.tanks.remove(this);
         }
 
+
         switch (dir) {
             case LEFT:
-                g.drawImage(ResourceMgr.tankL, x, y, null);
+                g.drawImage(this.group == Group.GOOD ? ResourceMgr.goodTankL : ResourceMgr.badTankL, x, y, null);
                 break;
             case UP:
-                g.drawImage(ResourceMgr.tankU, x, y, null);
+                g.drawImage(this.group == Group.GOOD ? ResourceMgr.goodTankU : ResourceMgr.badTankU, x, y, null);
                 break;
             case RIGHT:
-                g.drawImage(ResourceMgr.tankR, x, y, null);
+                g.drawImage(this.group == Group.GOOD ? ResourceMgr.goodTankR : ResourceMgr.badTankR, x, y, null);
                 break;
             case DOWN:
-                g.drawImage(ResourceMgr.tankD, x, y, null);
+                g.drawImage(this.group == Group.GOOD ? ResourceMgr.goodTankD : ResourceMgr.badTankD, x, y, null);
                 break;
             default:
                 break;
